61133edcbd398b3346a8d8f5d15fb3d4dae1d7ac
Ashley Norwood <ashleynorwood@Ashleys-MacBook-Pro.local>
2020-03-26 10:24:23 -0400

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,4 +1,25 @@
-cards = ["queen", "queen", "king", "king"];
+const cards = [
+{
+  rank: "queen",
+  suit: "hearts",
+  cardImage: "images/queen-of-hearts.png"
+},
+{
+  rank: "queen",
+  suit: "diamonds",
+  cardImage: "images/queen-of-diamonds.png"
+},
+{
+  rank: "king",
+  suit: "hearts",
+  cardImage: "images/king-of-hearts.png"
+},
+{
+  rank: "king",
+  suit: "diamonds",
+  cardImage: "images/king-of-diamonds.png"
+}
+]
 cardsInPlay = [];
 function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1])
@@ -11,13 +32,18 @@ function checkForMatch() {
   	}
   }
 function flipCard(cardID) {
-	console.log("User flipped " + cards[cardID]);
-	cardsInPlay.push(cards[cardID]);
+	console.log("User flipped " + cards[cardID].rank);
+	cardsInPlay.push(cards[cardID].rank);
+
+  console.log(cards[cardID].cardImage);
+  console.log(cards[cardID].suit);
 
 	if (cardsInPlay.length === 2) {
- 	checkForMatch(); 	
+ 	checkForMatch(); 
+
 	}
 }
 
 flipCard(0);
 flipCard(2);
+
